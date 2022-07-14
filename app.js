58dc265f60f5f59969b1f5a636ce6aca6c7e9d20
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-07-14 07:08:48 +0700

    7.08

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const http = require ("http");
+const http = require ('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+  res.setHeader = ('Content-Type', 'text/plain');
+  res.write = ('kmkpmkp');
+  res.end();
+
+})
+
+server.listen(3005, '127.0.0.1', console.log('Сервер запущен')
+)
