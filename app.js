82027c0575120757aac2347093b20abf1d891091
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-07-14 15:29:16 +0700

    15:29

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const http = require ('http');
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.statusMessage = 'OK';
-  res.setHeader = ('Content-Type', 'text/plain');
-  res.write = ('kmkpmkp');
-  res.end();
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("hello world");
+});
+app.listen(3005, () => {
+  console.log("Load");
+});
 
-})
+// const http = require ('http');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.statusMessage = 'OK';
+//   res.setHeader = ('Content-Type', 'text/plain');
+//   res.write = ('kmkpmkp');
+//   res.end();
 
-server.listen(3005, '127.0.0.1', console.log('Сервер запущен')
-)
+// })
+
+// server.listen(3005, '127.0.0.1', console.log('Сервер запущен')
+// )
