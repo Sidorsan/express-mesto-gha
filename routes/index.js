5e5de305d299e7643aaf1c5f8c471e61087ab0a7
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-07-19 11:16:06 +0700

    9:47

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
-
 router.use(userRouter);
 router.use(cardRouter);
 router.use((req, res) => {
