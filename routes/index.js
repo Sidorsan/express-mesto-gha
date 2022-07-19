fea2d0b64a18c2a3c311ab199b35e3fd7ef315a6
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-07-19 11:11:22 +0700

    9:45

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
+
 router.use(userRouter);
 router.use(cardRouter);
+router.use((req, res) => {
+  res.status(404).send({ message: 'Wrong URL' });
+});
 
 module.exports = router;
