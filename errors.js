35350ebe8b4a7dfae1e5813e63f0947d051f317a
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-07-20 14:22:37 +0700

    Add errors file2

--- errors.js
+++ errors.js
@@ -1,5 +1,5 @@
 module.exports.SERVER_ERROR_CODE = 500;
 module.exports.VALIDATION_ERROR_CODE = 400;
 module.exports.CAST_ERROR_CODE = 400;
-module.exports.CAST_ERROR_CODE = 404;
-module.exports.NOT_FOUND_ERROR_CODE = 400;
+module.exports.CAST_ERROR_CODE = 400;
+module.exports.NOT_FOUND_ERROR_CODE = 404;
