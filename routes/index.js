95868e736d87bc5c45f825455da31f80f58a9948
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-07-21 13:42:39 +0700

    Add errors file3

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
+const { NOT_FOUND_ERROR_CODE } = require('../errors');
 
 router.use(userRouter);
 router.use(cardRouter);
 router.use((req, res) => {
-  res.status(404).send({ message: 'Wrong URL' });
+  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Wrong URL' });
 });
 
 module.exports = router;
