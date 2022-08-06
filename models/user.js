56d7d721c7a4d97e67958915f42bd8401b97bffc
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-08-06 14:43:24 +0700

    14:43

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+
+const regex = new RegExp(/^(https||http):\/\//)
 const userSchema = new mongoose.Schema(
   {
     email: {
