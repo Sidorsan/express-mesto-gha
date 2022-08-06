bbb967d9f138a4cdf3d445cdd987aa43cd45603c
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-08-06 07:25:14 +0700

    7:13

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
+// const router = require('express').Router();
+// const userRouter = require('./users');
+// const cardRouter = require('./cards');
+// const NotFoundError  = require('../errors/NotFoundError');
+// const { NOT_FOUND_ERROR_CODE } = require('../errors/errors');
+
+// router.use(userRouter);
+// router.use(cardRouter);
+// router.use((req, res) => {
+//   // res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Wrong URL' });
+//   throw new NotFoundError('Wrong URL');
+// })
+
+// module.exports = router;
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const NotFoundError  = require('../errors/NotFoundError');
+const NotFoundError  = require('../errors');
+// const { NOT_FOUND_ERROR_CODE } = require('../errors');
+// const NotFoundError  = require('../errors/NotFoundError');
 const { NOT_FOUND_ERROR_CODE } = require('../errors/errors');
 
 router.use(userRouter);
@@ -9,6 +25,7 @@ router.use(cardRouter);
 router.use((req, res) => {
   // res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Wrong URL' });
   throw new NotFoundError('Wrong URL');
-})
+});
+
 
 module.exports = router;
