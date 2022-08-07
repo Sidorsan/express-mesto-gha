800a861c2f24faf633bf2dfe7cb8a9d0de939ad7
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-08-07 11:43:08 +0700

    8:03

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken');
 // const ValidationErrorCode = require('../errors/ValidationErrorCode');
 
 const {
-  CastErrorCode,
+  // CastErrorCode,
   ConflictErrorCode,
   ForbiddenErrorCode,
   NotFoundError,
@@ -19,6 +19,7 @@ const {
   ValidationErrorCode,
 } = require('../error');
 
+const CastErrorCode = require('../errors/CastErrorCode')
 // console.log(new CastErrorCode('ewce'));
 // const {
 //   SERVER_ERROR_CODE,
--- error.js
+++ error.js
@@ -6,53 +6,57 @@ module.exports.CONFLICT_ERROR_CODE = 409;//
 module.exports.FORBIDDEN_ERROR_CODE = 403;
 module.exports.UNAUTHORIZED_ERROR_CODE = 401;
 
+
+// const CAST_ERROR_CODE = 400
+// module.exports = {CAST_ERROR_CODE}
+
 // const {CAST_ERROR_CODE}= require('./errors');
- class CastErrorCode extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = CAST_ERROR_CODE;
-  }
-}
-module.exports = CastErrorCode
+// class CastErrorCode extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.statusCode = CAST_ERROR_CODE;
+//   }
+// }
+// module.exports = CastErrorCode
 
-// const {CONFLICT_ERROR_CODE }= require('./errors');
- class ConflictErrorCode extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = CONFLICT_ERROR_CODE;
-  }
-}
-module.exports = ConflictErrorCode
+// // const {CONFLICT_ERROR_CODE }= require('./errors');
+//  class ConflictErrorCode extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.statusCode = CONFLICT_ERROR_CODE;
+//   }
+// }
+// // module.exports = ConflictErrorCode
 
-// const {FORBIDDEN_ERROR_CODE }= require('./errors');
-module.exports = class ForbiddenErrorCode extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = FORBIDDEN_ERROR_CODE;
-  }
-}
+// // const {FORBIDDEN_ERROR_CODE }= require('./errors');
+//  class ForbiddenErrorCode extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.statusCode = FORBIDDEN_ERROR_CODE;
+//   }
+// }
 
-// const {NOT_FOUND_ERROR_CODE }= require('./errors');
-module.exports = class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = NOT_FOUND_ERROR_CODE;
-  }
-}
+// // const {NOT_FOUND_ERROR_CODE }= require('./errors');
+// class NotFoundError extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.statusCode = NOT_FOUND_ERROR_CODE;
+//   }
+// }
 
-// const {UNAUTHORIZED_ERROR_CODE }= require('./errors');
-module.exports = class UnauthorizedErrorCode extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = UNAUTHORIZED_ERROR_CODE;
-  }
-}
+// // const {UNAUTHORIZED_ERROR_CODE }= require('./errors');
+// class UnauthorizedErrorCode extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.statusCode = UNAUTHORIZED_ERROR_CODE;
+//   }
+// }
 
-// const {VALIDATION_ERROR_CODE}= require('./errors');
-module.exports = class ValidationErrorCode extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = VALIDATION_ERROR_CODE;
+// // const {VALIDATION_ERROR_CODE}= require('./errors');
+//  class ValidationErrorCode extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.statusCode = VALIDATION_ERROR_CODE;
 
-  }
-}
+//   }
+// }
