05713e4b16b5b900c212cedf4a31c6090d5795db
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-08-08 13:22:18 +0700

    13:05

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.post(
   celebrate({
     body: Joi.object()
       .keys({
-        email: Joi.string().required().min(2).max(30),
+        email: Joi.string().required().email(),
         password: Joi.string().required().min(2).max(30),
       })
       .keys({
