cc3d1afb7c0e0625d81b59859463c94cd1e1041a
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-08-10 20:16:07 +0700

    19:33

--- controllers/cards.js
+++ controllers/cards.js
@@ -44,13 +44,13 @@ module.exports.deleteCard = (req, res, next) => {
         next(new NotFoundError('Карточка не найдена'));
         return;
       }
-      if (req.user._id !== card.owner) {
+      if (req.user._id != card.owner) {
         next(
           new ForbiddenErrorCode('Нельзя удалить карточку другого пользователя')
         );
         return;
       }
-      return Card.deleteOne(card).then(() => res.status(200).send(card));
+      return  Card.deleteOne(card).then(() => res.status(200).send(card));
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -65,7 +65,7 @@ module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.id,
     { $addToSet: { likes: req.user._id } },
-    { new: true }
+    { new: true },
   )
     .then((card) => {
       if (!card) {
@@ -87,7 +87,7 @@ module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.id,
     { $pull: { likes: req.user._id } },
-    { new: true }
+    { new: true },
   )
     .then((card) => {
       if (!card) {
