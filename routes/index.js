4a9e815056361f52786c2743ffa2c1d91f620a0e
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-08-13 07:38:42 +0700

    7:37

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const { NotFoundError } = require('../errors/errorsCode');
+const { NotFoundError } = require('../errors/NotFoundError');
 
 router.use(userRouter);
 router.use(cardRouter);
