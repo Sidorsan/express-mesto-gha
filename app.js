0565b1429f2bed1b0973472999a0e056e46350b6
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-08-13 07:54:41 +0700

    7:54

--- app.js
+++ app.js
@@ -5,18 +5,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const router = require('./routes');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 app.use(bodyParser.json());
 app.use(router);
 
 app.use(errors());
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-  next();
-});
+app.use(errorHandler);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
