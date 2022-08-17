c33a3a26bc83548be7490eaa1e9ac4901e428d6c
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-08-17 14:04:59 +0700

    15 start

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const router = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(requestLogger);
 app.use(router);
 
+app.use(errorLogger);
 app.use(errors());
+
 app.use(errorHandler);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
