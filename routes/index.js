cd959a277ea6ec3526dfda151993671868f71f3a
Sidor007 <Sidor007@yandex.ru>
2022-08-17 20:35:25 +0700

    20:35

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 const { NotFoundError } = require('../errors/NotFoundError');
 
-router.use(userRouter);
-router.use(cardRouter);
+// router.use(userRouter);
+// router.use(cardRouter);
 router.use(() => {
   throw new NotFoundError('Wrong URL');
 });
