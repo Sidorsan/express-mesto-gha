7bb7b2edc7066afe388ce65700217512e928d52d
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-08-19 10:43:15 +0700

    addCors

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const mongoose = require('mongoose');
 const router = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { allowedCors } = require('./middlewares/cors');
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(router);
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
 
 app.use(errorLogger);
 app.use(errors());
