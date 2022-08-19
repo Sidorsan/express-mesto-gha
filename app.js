5cf4ba78f96dbf4f0b2b743481f349d410b34b1a
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-08-19 10:55:23 +0700

    addCors1

--- app.js
+++ app.js
@@ -18,8 +18,17 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    const { method } = req;
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      const requestHeaders = req.headers['access-control-request-headers'];
+      if (method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Headers', requestHeaders);
+        return res.end();
+      }
+    }
   }
-
   next();
 });
 
