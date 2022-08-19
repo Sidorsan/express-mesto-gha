7061d39ff116027f49d4471f7bc77f2851d5c030
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-08-19 14:24:51 +0700

    addCors7

--- app.js
+++ app.js
@@ -15,20 +15,20 @@ app.use(bodyParser.json());
 app.use(requestLogger);
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    const { method } = req;
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      const requestHeaders = req.headers['access-control-request-headers'];
       if (method === 'OPTIONS') {
         res.header('Access-Control-Allow-Headers', requestHeaders);
         return res.end();
       }
     }
   }
-  next();
+  return next();
 });
 
 app.use(router);
