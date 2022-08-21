e8ca257785987c5370a23c4cb2725882a04dddb6
Sidor007 <Sidor007@yandex.ru>
2022-08-22 06:56:06 +0700

    6:56

--- routes/cards.js
+++ routes/cards.js
@@ -20,7 +20,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().pattern(regexUrlCheck),
+      link: Joi.string().required(),
     }),
   }),
   createCard,
