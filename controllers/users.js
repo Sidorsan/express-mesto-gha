35beff780de53249055a9c4f700baf003f23b7ee
Sidor007 <Sidor007@yandex.ru>
2022-08-22 14:51:35 +0700

    14:51

--- controllers/users.js
+++ controllers/users.js
@@ -142,7 +142,7 @@ module.exports.login = (req, res, next) => {
           return;
         }
 
-        const tokenUser = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET, {
+        const tokenUser = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', {
           expiresIn: '7d',
         });
         res.status(200).send({ token: tokenUser });
